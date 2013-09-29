4c0588873a9ffe3b1b6e7bc140bd5f2ec33a6035
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2013-09-29 14:12:39 +0200

    Elsődleges kommit. Gitignore a LocalSettings.php fájlnak. includes.php és msupload.php létrehozva.

---
+++ includes.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once('msupload.php');
+
---
+++ msupload.php
@@ -0,0 +1,8 @@
+<?php
+
+$wgMSU_ShowAutoKat = false;     #autocategorisation
+$wgMSU_CheckedAutoKat = false;  #checkbox: checked = true/false
+$wgMSU_debug = false;
+$wgMSU_ImgParams = '400px';     #default max-size for image insert
+$wgMSU_UseDragDrop = true;      #show Drag&Drop Area?
+require_once("$IP/extensions/MsUpload/msupload.php");
